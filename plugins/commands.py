f626f76c6c61a0f7e172b1719dc52725fb2abea9
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2022-11-21 19:43:34 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -242,7 +242,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ArrowFlix', url='https://t.me/ArrowFlix') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
