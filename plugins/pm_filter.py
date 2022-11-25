f752fc6aebc348c0d6d20b0c7172bcb35c2145de
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2022-11-25 21:05:23 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -368,7 +368,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer('Check PM, I have sent files in pm', show_alert=True)
+                await query.answer('Check PM, I have sent files in pm/nتم ارساله علي الخاص', show_alert=True)
         except UserIsBlocked:
             await query.answer('Unblock the bot mahn !', show_alert=True)
         except PeerIdInvalid:
